808588171aa0e1415a8ec6bbb5ae27a3372a9aef
Dhruv3sh <dhruv.3sh@gmail.com>
2025-12-23 16:07:55 +0530

    first commit

---
+++ Ams.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//** implemented swap function of int type */
+void my_swap(int& i1, int& i2){
+    int temp = i1;
+    i1 = i2;
+    i2 = temp;
+}
+
+void revArray(int size, int arr[]){
+    int n = size-1;
+    for(int i=0; i<size/2; i++){
+        my_swap(arr[i], arr[n]);
+        n--;
+    }
+}
+
+int main(){
+   int arr[] = {2,4,6,8,9,10,16,18,20,25,28,30};
+   int size = sizeof(arr)/sizeof(arr[0]);
+   
+   revArray(size ,arr);
+
+   for(int i=0; i<size; i++){
+    cout<<arr[i]<<" ";
+   };
+    return 0;
+}

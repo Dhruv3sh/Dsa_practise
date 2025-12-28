d7b8ccd13d6598f4a9bd8f2eea9ab532cb218b6b
Dhruv3sh <dhruv.3sh@gmail.com>
2025-12-28 21:50:28 +0530

    switch value without third variable

---
+++ exchangeValues.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a= 16,b=15;
+
+    a = a+b;
+    b= a-b;
+    a= a-b;
+
+    cout<<"a : "<<a<<endl;
+    cout<<"b : "<<b;
+
+    return 0;
+}

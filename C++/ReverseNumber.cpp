afcffc82af2e9ed4b56fa2348582725fcf9cd67f
Dhruv3sh <dhruv.3sh@gmail.com>
2026-01-10 01:28:09 +0530

    Reversing number using simple math

---
+++ C++/ReverseNumber.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    int revNum = 0;
+    while(n>0){
+        int ld = n%10;
+        n /= 10;
+        revNum = (revNum * 10) + ld;
+    }
+
+    cout<< revNum;
+    return 0;
+}

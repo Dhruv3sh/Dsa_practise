5925279d3b125596e5bacb8479f5a259087cd379
Dhruv Sharma <112899026+Dhruv3sh@users.noreply.github.com>
2026-01-21 22:52:18 +0530

    Initialize ArrayList with predefined values

--- Java/Practise.java
+++ Java/Practise.java
@@ -2,11 +2,7 @@ import java.util.ArrayList;
 
 public class Practise {
     public static void main(String[] args){
-        ArrayList<Integer> obj = new ArrayList<>();
-
-        obj.add(3);
-        obj.add(4);
-        obj.add(5);
+        ArrayList<Integer> obj = new ArrayList<>(List.of(3,4,6,8));
         for(int i=0; i<obj.size(); i++){
             System.out.println(obj.get(i));
         };

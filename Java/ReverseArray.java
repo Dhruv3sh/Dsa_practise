c85ef43e7dedc1510119ea5b85ce0f33abcfd0a1
Dhruv Sharma <112899026+Dhruv3sh@users.noreply.github.com>
2026-01-23 17:17:22 +0530

    Implement ReverseArray function to reverse an array

---
+++ Java/ReverseArray.java
@@ -0,0 +1,17 @@
+﻿public static void ReverseArray(int[] arr) {
+    int start = 0;
+    int end = arr.length - 1;
+
+
+    while (start < end) {
+        // Swap elements at start and end indices
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+
+        // Move pointers inward
+        start++;
+        end--;
+    }
+}

2872f82146f7683a15da915330bfdcbfb83eed9e
Dhruv Sharma <112899026+Dhruv3sh@users.noreply.github.com>
2026-01-30 19:13:56 +0530

    reverse logic in ReverseArray.java
    Commented out the reverse array logic and added a note for the two pointer approach.

--- Java/ReverseArray.java
+++ Java/ReverseArray.java
@@ -27,6 +27,10 @@ public class ReverseArray {
             System.out.print(arr[i] + " ");
         }
 
+        //for (int i = arr.length - 1; i >= 0; i--) {
+         //   System.out.print(arr[i] + " ");
+        //}
+
         //*Two pointer approach */
         // int start = 0;
         // int end = arr.length - 1;

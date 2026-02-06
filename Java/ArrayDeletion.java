91e7f1ee541ba63ac237fe195c3a37d55cfa0a1c
DhruvSh <Dhruv.3sh@gmail.com>
2026-02-06 23:22:59 +0530

    Deleting and shifting array elemnts to the left

---
+++ Java/ArrayDeletion.java
@@ -0,0 +1,20 @@
+class ArrayDeletion {
+    public static void main(String[] args){
+        int[] arr = {10,12,14,16,18};
+        int size = arr.length;
+
+        while(size > 0){
+            for(int i = 0; i<size -1; i++){
+                arr[i] = arr[i+1];
+            }
+            size--;
+            
+            for(int i = 0; i<size; i++){
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
+
+        
+    }
+}

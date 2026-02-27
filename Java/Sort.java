992858c426f748374442043238a0a3c5d8eaebe8
DhruvSh <Dhruv.3sh@gmail.com>
2026-02-28 00:53:34 +0530

    Added more fnc

--- Java/Sort.java
+++ Java/Sort.java
@@ -1,4 +1,18 @@
 class Sort {
+    void SelectionSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int min = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min])
+                    min = j;
+            }
+            ;
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+
     void BubbleSort(int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - 1 - i; j++) {
@@ -11,15 +25,41 @@ class Sort {
         }
     }
 
+    void InsertionSort(int[] arr){
+        for(int i=0; i<=arr.length-1; i++){
+            int j=i;
+            while(j>0 && arr[j-1] > arr[j]){
+                int temp = arr[j-1];
+                arr[j-1] = arr[j];
+                arr[j] = temp;
+                j--;
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        int[] arr = { 5, 3, 8, 2 };
+        int[] arr = { 5, 3, 8, 2, 6, 9 };
 
         Sort obj = new Sort();
-        obj.BubbleSort(arr);
 
+        obj.SelectionSort(arr);
+        System.out.println("Selection sort:");
         for (int i = 0; i < arr.length; i++) {
-            System.out.print("Bubble sort : " + arr[i] + " ");
+            System.out.print(arr[i] + " ");
         }
 
+        System.out.println();
+        obj.BubbleSort(arr);
+        System.out.println("Bubble sort:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.println();
+        obj.InsertionSort(arr);
+        System.out.println("Insertion sort:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }

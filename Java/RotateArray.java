98b58023693b1393773d0774a397ff3ca170c8e2
DhruvSh <Dhruv.3sh@gmail.com>
2026-03-05 23:57:55 +0530

    cmnt

--- Java/RotateArray.java
+++ Java/RotateArray.java
@@ -2,7 +2,7 @@ public class RotateArray {
     public static void main(String[] args) {
 
         int[] arr = {1,2,3,4,5};
-        int k = 2;
+        int k = 2; //point to rotate array
 
         for(int i=0;i<k;i++){
 

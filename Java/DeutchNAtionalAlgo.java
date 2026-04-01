402dfb5f47b29607a9e81ba1a0fb9584b23584e1
DhruvSh <Dhruv.3sh@gmail.com>
2026-04-01 21:41:14 +0530

    arrange 0,1 and 2 in sorted order

---
+++ Java/DeutchNAtionalAlgo.java
@@ -0,0 +1,30 @@
+public class DeutchNAtionalAlgo {
+    public static void main(String[] args) {
+        int[] arr = { 0, 1, 1, 0, 1, 2, 2, 0};
+        int low = 0;
+        int mid = 0;
+        int high = arr.length - 1;
+
+
+        while (mid <= high) {
+            if (arr[mid] == 0) {
+                int temp = arr[mid];
+                arr[mid] = arr[low];
+                arr[low] = temp;
+                low++;
+                mid++;
+            } else if (arr[mid] == 1) {
+                mid++;
+            } else {
+                int temp = arr[mid];
+                arr[mid] = arr[high];
+                arr[high] = temp;
+                high--;
+            }
+        };
+
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}

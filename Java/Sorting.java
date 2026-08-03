f12e0e7b4b904e8285ad3058ad8f6bde4d774125
Dhruv3sh <dhruv.3sh@gmail.com>
2026-08-03 19:50:09 +0530

    sorting in java

---
+++ Java/Sorting.java
@@ -0,0 +1,17 @@
+public class Sorting {
+    public void Merge(){
+
+    }
+
+    public static void MergeSort(int[] arr, int low, int high){
+        
+    }
+
+    public static void main(String[] args){
+        int[] arr = {2,5,1,3,8,6,4};
+        int high = arr.length;
+
+        MergeSort(arr,0,high);
+
+    }
+}
